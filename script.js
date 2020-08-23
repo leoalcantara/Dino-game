569bdf606db0c90378eea24b0f10e4ade05f26b4
leoalcantara <leonidio.ti@gmail.com>
2020-08-23 14:17:31 -0300

    inserted images

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const dino = document.querySelector('.dino');
-let isJumping = false;
+const background = document.querySelector('.background');
 
+let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event){
     if (event.keyCode ===32){
@@ -10,8 +12,6 @@ function handleKeyUp(event){
     }
 }
 function jump(){
-    let position = 0;
-
     let upInterval = setInterval(() => {
         if (position >= 150){
             clearInterval(upInterval);
@@ -34,4 +34,32 @@ function jump(){
     }, 20);
 }
 
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    background.appendChild(cactus);
+    cactus.style.left = cactusPosition +'px';
+    
+
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+                clearInterval(leftInterval);
+                document.body.innerHTML = '<H1 class="game-over">Fim de Jogo</H1>'
+            } else{
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+            }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp);
